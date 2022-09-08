8c09606c9ef8f12932fa8aeab05825b7b9a40c9a
LuisaArellano <42380922+LuisaArellano@users.noreply.github.com>
2022-09-07 20:19:20 -0500

    subi pag web v2

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
-
-const express = require('express');
-const { read } = require('fs');
+// importar las bibliotecas
+const express = require('express'); // es opcional el ';'
 const path = require('path');
+
 const app = express();
-
-app.use(express.static(path.join(__dirname,'Public')));  // va a estar disponible en la carpeta Public
-
-// def de la app
-app.get('/bigote', (request, response) =>{
-    console.log('Hola mundo');
-    response.send('<h1>Hola mundo</h1>')
-
+// middleware
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+// definición de nuestra aplicación
+app.get('/bigote', (request, response) => {
+    console.log('Hola hola');
+    response.send('<H1> Hola Hola </H1>')
 });
 
-app.get('/',(req, res)=>{
-    res.sendFile(path.join(__dirname,'views', 'index.html'));
-    
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
-//prueba query
-app.get('/prueba1', (req, res)=>{
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'pagina1.html'));
+})
+
+
+//prueba
+app.get('/prueba', (req, res) => {
     console.log(req.query)
-    res.send("datos enviados por query"+req.query.name)
-})
+    res.send("Datos enviados por Query" + req.query.name)
+});
 
 //prueba params
-app.get('/prueba2/:name/:age',(req,res)=>{
-    console.log(req.params)
-    console.log(req.query)
-    res.send("datos enviados por params"+req.params.name)
-
-})
-app.post('/prueba3',(req,res) =>{
-    console.log(req.body);
-    res.send("datos recibidos")
-
+app.get('/prueba2/:name/:age',(req, res)=> {
+    console.log(req.params);
+    console.log(req.query);
+    console,console.log(req.params)
+    res.send('Datos enviados por params'+req.params.name)
 });
 
-//lanzar la app
-app.listen(8080,()=>{
-    console.log("Servidor en línea");
-
+app.get('/prueba3', (req, res) => {
+    console.log(req.boody)
+    res.send("Datos recibidos")
 });
-   
 
-
-
+// lanzar la aplicación
+app.listen(8081, () => {
+    console.log("Servidor en línea :)")
+});
